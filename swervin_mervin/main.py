204172e1ab534e5ebe24b3c9a017b5caec1d4b05
Andy Buntine <bunts@hhd.com.au>
2014-06-07 15:20:22 +1000

    Wow... This is hard

--- swervin_mervin/main.py
+++ swervin_mervin/main.py
@@ -12,21 +12,31 @@ pygame.init()
 fps            = 30
 dimensions     = (640, 480)
 road_width     = 2000
-speed          = 4 # TODO: Change to 0 once accel/decel implemented.
-acceleration   = 4
+speed          = 3 # TODO: Change to 0 once accel/decel implemented.
+acceleration   = 3
 white          = pygame.Color(255, 255, 255)
 light_grey     = pygame.Color(193, 193, 193)
 dark_grey      = pygame.Color(123, 123, 123)
 
 fps_clock = pygame.time.Clock()
 window    = pygame.display.set_mode(dimensions)
-window.fill(white)
+
+height = 50
 
 while True:
+    window.fill(white)
+
+    for n in range(10):
+        color = dark_grey if (n % 2 == 0) else light_grey
+        pygame.draw.rect(window, color, (0, (480 - height), 640, height), 0)
+        height = (height - acceleration);
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    height = 50
 
     pygame.display.update()
     fps_clock.tick(fps)
