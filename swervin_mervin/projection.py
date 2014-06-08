2cb2588b06c17fb2fdf6ffc2943c3532e4e20668
Andy Buntine <bunts@hhd.com.au>
2014-06-08 20:28:24 +1000

    Added projection function. Untested

--- swervin_mervin/projection.py
+++ swervin_mervin/projection.py
@@ -18,4 +18,14 @@ def find_segment(z, segments, segment_length):
     return segments[math.trunc((z / segment_length) % len(segments))]
 
 def project_line(segment, line, camera_x, camera_y, camera_z, camera_depth, width, height, road_width):
-    pass
+    p = segment[line]
+
+    p["camera"]["x"] = p["world"].get("x", 0) - camera_x
+    p["camera"]["y"] = p["world"].get("y", 0) - camera_y
+    p["camera"]["z"] = p["world"].get("z", 0) - camera_z
+    p["screen"]["s"] = camera_depth / p["camera"]["z"]
+    p["screen"]["x"] = math.trunc((width / 2) + (p["screen"]["s"] * p["camera"]["x"] * (width / 2)))
+    p["screen"]["y"] = math.trunc((height / 2) + (p["screen"]["s"] * p["camera"]["y"] * (height / 2)))
+    p["screen"]["w"] = math.trunc(p["screen"]["s"] * road_width * (width / 2))
+
+    return p
