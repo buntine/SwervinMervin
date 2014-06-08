38097d88ab2de459118247abd209fec0952922df
Andy Buntine <bunts@hhd.com.au>
2014-06-08 20:39:09 +1000

    Cleanup, bug fixes

--- swervin_mervin/main.py
+++ swervin_mervin/main.py
@@ -3,7 +3,7 @@
 # (c) Andrew Buntine
 # https://github.com/buntine/swervin_mervin
 
-import pygame, sys
+import pygame, sys, math
 from pygame.locals import *
 from projection import *
 
@@ -16,14 +16,15 @@ dimensions     = (640, 480)
 segment_height = 200
 rumble_length  = 3
 speed          = 0
-player_x       = 0
-player_z       = 0
 draw_distance  = 100
 road_width     = 1500
 top_speed      = (segment_height / (1.0/fps))
 acceleration   = top_speed / 5.0
 field_of_view  = 100 # Degrees
-camera_depth   = 1 / Math.tan((field_of_view / 2) * math.pi / 180);
+camera_height  = 1000
+camera_depth   = 1 / math.tan((field_of_view / 2) * math.pi / 180);
+player_x       = 0
+player_z       = camera_height * camera_depth
 colours        = {"white": pygame.Color(255, 255, 255),
                   "light_grey": pygame.Color(193, 193, 193),
                   "dark_grey": pygame.Color(123, 123, 123)}
@@ -51,14 +52,16 @@ while True:
     for s in range(draw_distance):
         index             = (base_segment["index"] + s) % len(segments)
         segment           = segments[index]
-        segment["bottom"] = project_segment(segment, "bottom")
-        segment["top"]    = project_segment(segment, "top")
+
+        segment["bottom"] = project_line(segment, "bottom", (player_x * road_width), camera_height, position, camera_depth, dimensions, road_width)
+        segment["top"]    = project_line(segment, "top", (player_x * road_width), camera_height, position, camera_depth, dimensions, road_width)
         segments[index]   = segment
 
         # Segment is behind us.
         if segment["bottom"]["camera"]["z"] <= camera_depth:
             continue
 
+        print segments[index]
         render_segment(segment)
 
     for event in pygame.event.get():
