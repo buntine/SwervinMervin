9b72d804e1ac7cc1b7cafc67da0537b408688a19
Andy Buntine <bunts@hhd.com.au>
2014-06-09 21:45:15 +1000

    Started refactoring

--- swervin_mervin/main.py
+++ swervin_mervin/main.py
@@ -3,66 +3,47 @@
 # (c) Andrew Buntine
 # https://github.com/buntine/swervin_mervin
 
-import pygame, sys, math
+import pygame, sys
 from pygame.locals import *
 from projection import *
 from rendering import *
+import settings as s
 
 pygame.init()
 
 # Game variables.
-fps            = 50
-position       = 0
-dimensions     = (640, 480)
-segment_height = 150
-rumble_length  = 3
-speed          = 1
-draw_distance  = 100
-road_width     = 1500
-top_speed      = (segment_height / (1.0/fps))
-acceleration   = top_speed / 9.0
-field_of_view  = 100 # Degrees
-camera_height  = 1400
-camera_depth   = 1 / math.tan((field_of_view / 2) * math.pi / 180);
-player_x       = 0
-direction_x    = 0
-player_z       = camera_height * camera_depth
-colours        = {"white": pygame.Color(255, 255, 255),
-                  "light": {"road": pygame.Color(193, 193, 193),
-                            "grass": pygame.Color(61, 212, 76),
-                            "rumble": pygame.Color(223, 215, 1),
-                            "line": pygame.Color(255, 255, 255)},
-                  "dark": {"road": pygame.Color(173, 173, 173),
-                           "grass": pygame.Color(50, 186, 62),
-                           "rumble": pygame.Color(192, 186, 0),
-                           "line": pygame.Color(255, 255, 255)}}
-
-segments       = build_segments(segment_height, rumble_length, colours)
-track_length   = len(segments) * segment_height
+position     = 0
+speed        = 1
+player_x     = 0
+direction_x  = 0
+player_z     = s.CAMERA_HEIGHT * s.CAMERA_DEPTH
+segments     = build_segments(s.SEGMENT_HEIGHT, s.RUMBLE_LENGTH, s.COLOURS)
+track_length = len(segments) * s.SEGMENT_HEIGHT
 
 fps_clock = pygame.time.Clock()
-window    = pygame.display.set_mode(dimensions)
+window    = pygame.display.set_mode(s.DIMENSIONS)
 
 while True:
-    window.fill(colours["white"])
+    window.fill(s.COLOURS["white"])
 
     position += (0.02 * speed)
-    speed += (acceleration * 0.02) # TODO: Might need actually time diff instead of 0.02 guess.
+    speed    += (s.ACCELERATION * 0.02) # TODO: Might need actually time diff instead of 0.02 guess.
     player_x += direction_x
 
+    # TODO: Move.
     while position >= track_length:
         position -= track_length
-
     while position < 0:
         position += track_length
 
-    if speed > top_speed:
-        speed = top_speed
+    # TODO: Move.
+    if speed > s.TOP_SPEED:
+        speed = s.TOP_SPEED
 
-    base_segment = find_segment(position, segments, segment_height)
+    base_segment = find_segment(position, segments, s.SEGMENT_HEIGHT)
 
-    for s in range(draw_distance):
-        index              = (base_segment["index"] + s) % len(segments)
+    for i in range(s.DRAW_DISTANCE):
+        index              = (base_segment["index"] + i) % len(segments)
         segment            = segments[index]
         projected_position = position
 
@@ -70,16 +51,16 @@ while True:
         if segment["index"] < base_segment["index"]:
             projected_position -= track_length
 
-        project_line(segment, "top", (player_x * road_width), camera_height, projected_position, camera_depth, dimensions, road_width)
-        project_line(segment, "bottom", (player_x * road_width), camera_height, projected_position, camera_depth, dimensions, road_width)
+        project_line(segment, "top", (player_x * s.ROAD_WIDTH), s.CAMERA_HEIGHT, projected_position, s.CAMERA_DEPTH, s.DIMENSIONS, s.ROAD_WIDTH)
+        project_line(segment, "bottom", (player_x * s.ROAD_WIDTH), s.CAMERA_HEIGHT, projected_position, s.CAMERA_DEPTH, s.DIMENSIONS, s.ROAD_WIDTH)
 
         # Segment is behind us.
-        if segment["bottom"]["camera"]["z"] <= camera_depth:
+        if segment["bottom"]["camera"]["z"] <= s.CAMERA_DEPTH:
             continue
 
-        render_grass(window, segment, dimensions)
-        render_road(window, segment, dimensions, rumble_length)
-        render_player(window, segment, dimensions)
+        render_grass(window, segment, s.DIMENSIONS)
+        render_road(window, segment, s.DIMENSIONS, s.RUMBLE_LENGTH)
+        render_player(window, segment, s.DIMENSIONS)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -87,19 +68,19 @@ while True:
             sys.exit()
         elif event.type == KEYDOWN:
             if event.key == K_LEFT:
-                direction_x = -(0.02 * 2 * (speed / top_speed))
+                direction_x = -(0.02 * 2 * (speed / s.TOP_SPEED))
             elif event.key == K_RIGHT:
-                direction_x = (0.02 * 2 * (speed / top_speed))
+                direction_x = (0.02 * 2 * (speed / s.TOP_SPEED))
         else:
             direction_x = 0
 
+    # TODO: Move.
     # Prevent player from going too far off track.
     if player_x < -1.8:
         player_x = -1.8
-
-    if player_x > 1.8:
+    elif player_x > 1.8:
         player_x = 1.8
 
 
     pygame.display.update()
-    fps_clock.tick(fps)
+    fps_clock.tick(s.FPS)
---
+++ swervin_mervin/settings.py
@@ -0,0 +1,25 @@
+# Game constants.
+
+from pygame import Color
+import math
+
+FPS              = 50
+DIMENSIONS       = (640, 480)
+SEGMENT_HEIGHT   = 150
+RUMBLE_LENGTH    = 3
+DRAW_DISTANCE    = 100
+ROAD_WIDTH       = 1500
+TOP_SPEED        = (SEGMENT_HEIGHT / (1.0/FPS))
+ACCELERATION     = TOP_SPEED / 9.0
+FIELD_OF_VIEW    = 100 # Degrees
+CAMERA_HEIGHT    = 1400
+CAMERA_DEPTH     = 1 / math.tan((FIELD_OF_VIEW / 2) * math.pi / 180);
+COLOURS          = {"white": Color(255, 255, 255),
+                    "light": {"road":   Color(193, 193, 193),
+                              "grass":  Color(61, 212, 76),
+                              "rumble": Color(223, 215, 1),
+                              "line":   Color(255, 255, 255)},
+                    "dark":  {"road":   Color(173, 173, 173),
+                              "grass":  Color(50, 186, 62),
+                              "rumble": Color(192, 186, 0),
+                              "line":   Color(255, 255, 255)}}
