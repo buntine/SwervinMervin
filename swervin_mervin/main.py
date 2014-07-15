20bfd5c6863a2e14dc739828678833b2d5c52119
Andy Buntine <bunts@hhd.com.au>
2014-07-15 23:19:16 +1000

    Added pseudo-code for new game flow

--- swervin_mervin/main.py
+++ swervin_mervin/main.py
@@ -13,4 +13,39 @@ pygame.init()
 window = pygame.display.set_mode(s.DIMENSIONS)
 level  = l.Level("melbourne")
 
+# Maybe each stage shuldbe a class that can .setup() and .progress() while .alive() is true?
+
 title_sequence(window, level)
+
+#ts = title_screen(window)
+
+#while True:
+#    p  = Player()
+#    l  = Level()
+#    g  = Game(window, p, l)
+
+#    Fire up the title screen.
+#    ts.setup()
+#    while not ts.finished:
+#        ts.progress()
+#    ts.clean()
+
+#    Play the game.
+#    g.setup()
+#    while not g.finished:
+#        g.progress()
+#    g.clean()
+
+#    Enter name for high score entry.
+#    if p.high_score():
+#        h = HighScore(window, p, l)
+#        h.setup()
+#        while not h.finished():
+#            h.progress()
+#        h.clean()
+
+#    Resume game in "game over" mode, waiting for new player.
+#    g.game_over()
+#    while not g.finished:
+#        g.progress()
+#    g.clean()
