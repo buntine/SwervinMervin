87a69d1a583693926ae0335d9e032b9d0af8e735
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-01-21 23:02:23 +1100

    Finished refactor pre-pause

--- swervin_mervin/game.py
+++ swervin_mervin/game.py
@@ -16,6 +16,9 @@ class Game:
         self.clock       = clock
         self.paused      = False
         self.waiting     = False
+        self.player      = None
+        self.level       = None
+        self.backgrounds = None
         self.high_scores = hs.HighScores()
 
     def play(self):
@@ -25,117 +28,29 @@ class Game:
         if s.COUNTDOWN:
             self.__countdown();
 
-        player      = p.Player(self.high_scores.minimum_score())
-        level       = l.Level("melbourne")
-        backgrounds = [b.Background("sky", 0, 2, True),
-                        b.Background("city", 0, 1)]
+        self.player      = p.Player(self.high_scores.minimum_score())
+        self.level       = l.Level("melbourne")
+        self.backgrounds = [b.Background("sky", 0, 2, True),
+                            b.Background("city", 0, 1)]
 
-        level.build()
+        self.level.build()
 
         pygame.mixer.music.load(os.path.join("lib", "lazerhawk-overdrive.ogg"))
         pygame.mixer.music.play(-1)
         pygame.mixer.music.set_volume(s.MUSIC_VOLUME)
 
-        while player.alive():
-            player.travel(level.track_length(), self.window)
-
-            base_segment   = level.find_segment(player.position)
-            player_segment = level.find_segment(player.position + s.PLAYER_Z)
-
-            player.accelerate()
-            player.steer(player_segment)
-            player.climb(base_segment)
-            player.detect_collisions(player_segment)
-            player.handle_crash()
-
-            # Sprinkle some random bonuses into the next lap if we are lucky.
-            if player.new_lap:
-                if random.randint(1, s.CHANCE_OF_BONUSES) == 1:
-                    level.insert_bonuses()
-
-            # Move the other players.
-            for c in level.competitors:
-                old_seg = level.find_segment(c.position)
-                c.travel(level.track_length())
-                new_seg = level.find_segment(c.position)
-
-                if old_seg.index != new_seg.index:
-                    if c in old_seg.competitors:
-                        old_seg.competitors.remove(c)
-                    new_seg.competitors.append(c)
-
-            y_coverage  = 0
-            curve       = 0
-            curve_delta = -(base_segment.curve * player.segment_percent())
-
-            # Position backgrounds according to current curve.
-            for bg in backgrounds:
-                if base_segment.curve != 0:
-                    bg.step(base_segment.curve, player.speed_percent())
-                bg.render(self.window)
-
-            # Loop through segments we should draw for this frame.
-            for i in range(s.DRAW_DISTANCE):
-                segment            = level.offset_segment(base_segment.index + i)
-                projected_position = player.position
-                camera_x           = player.x * s.ROAD_WIDTH
-
-                # Past end of track and looped back.
-                if segment.index < base_segment.index:
-                    projected_position -= level.track_length()
-
-                segment.project(camera_x,
-                  curve,
-                  curve_delta,
-                  projected_position,
-                  player.y)
-
-                curve       += curve_delta
-                curve_delta += segment.curve
-
-                # Remember highest Y coordinate so we can clip sprites later.
-                segment.clip = y_coverage
-
-                if segment.should_ignore(y_coverage):
-                    continue
-
-                segment.render_grass(self.window)
-                segment.render_road(self.window)
-
-                if (segment.top["screen"]["y"] > y_coverage):
-                    y_coverage = segment.top["screen"]["y"]
-
-            # Draw sprites in from back to front (painters algorithm).
-            if segment.index != base_segment.index:
-                for i in reversed(range(1, s.SPRITE_DRAW_DISTANCE)):
-                    segment = level.offset_segment(base_segment.index + i)
-                    segment.render_world_objects(self.window)
-
-                player.render(self.window, base_segment)
-                player.render_hud(self.window)
-
-                if player.blood_alpha > 0:
-                    player.render_blood(self.window)
-
-            for event in pygame.event.get():
-                if event.type == QUIT or\
-                   (event.type == pygame.KEYDOWN and\
-                    event.key == pygame.K_ESCAPE and\
-                    s.FULLSCREEN):
-                    pygame.quit()
-                    sys.exit()
-
-            # Steering, acceleration.
-            keys = pygame.key.get_pressed()
-            player.set_acceleration(keys)
-            player.set_direction(keys)
+        while self.player.alive():
+            if self.paused:
+                self.__pause_cycle()
+            else:
+                self.__game_cycle()
 
             pygame.display.update()
             self.clock.tick(s.FPS)
 
         ## Post-game high scores and wait for new player.
-        if self.high_scores.is_high_score(player.points):
-            self.high_scores.add_high_score(math.trunc(player.points))
+        if self.high_scores.is_high_score(self.player.points):
+            self.high_scores.add_high_score(math.trunc(self.player.points))
 
         pygame.mixer.music.fadeout(1500)
         self.waiting = True
@@ -174,6 +89,106 @@ class Game:
      
             self.clock.tick(s.FPS)
 
+    def __game_cycle(self):
+        p = self.player
+        l = self.level
+
+        p.travel(l.track_length(), self.window)
+
+        base_segment   = l.find_segment(p.position)
+        player_segment = l.find_segment(p.position + s.PLAYER_Z)
+
+        p.accelerate()
+        p.steer(player_segment)
+        p.climb(base_segment)
+        p.detect_collisions(player_segment)
+        p.handle_crash()
+
+        # Sprinkle some random bonuses into the next lap if we are lucky.
+        if p.new_lap:
+            if random.randint(1, s.CHANCE_OF_BONUSES) == 1:
+                l.insert_bonuses()
+
+        # Move the other players.
+        for c in l.competitors:
+            old_seg = l.find_segment(c.position)
+            c.travel(l.track_length())
+            new_seg = l.find_segment(c.position)
+
+            if old_seg.index != new_seg.index:
+                if c in old_seg.competitors:
+                    old_seg.competitors.remove(c)
+                new_seg.competitors.append(c)
+
+        y_coverage  = 0
+        curve       = 0
+        curve_delta = -(base_segment.curve * p.segment_percent())
+
+        # Position backgrounds according to current curve.
+        for bg in self.backgrounds:
+            if base_segment.curve != 0:
+                bg.step(base_segment.curve, p.speed_percent())
+            bg.render(self.window)
+
+        # Loop through segments we should draw for this frame.
+        for i in range(s.DRAW_DISTANCE):
+            segment            = l.offset_segment(base_segment.index + i)
+            projected_position = p.position
+            camera_x           = p.x * s.ROAD_WIDTH
+
+            # Past end of track and looped back.
+            if segment.index < base_segment.index:
+                projected_position -= l.track_length()
+
+            segment.project(camera_x,
+              curve,
+              curve_delta,
+              projected_position,
+              p.y)
+
+            curve       += curve_delta
+            curve_delta += segment.curve
+
+            # Remember highest Y coordinate so we can clip sprites later.
+            segment.clip = y_coverage
+
+            if segment.should_ignore(y_coverage):
+                continue
+
+            segment.render_grass(self.window)
+            segment.render_road(self.window)
+
+            if (segment.top["screen"]["y"] > y_coverage):
+                y_coverage = segment.top["screen"]["y"]
+
+        # Draw sprites in from back to front (painters algorithm).
+        if segment.index != base_segment.index:
+            for i in reversed(range(1, s.SPRITE_DRAW_DISTANCE)):
+                segment = l.offset_segment(base_segment.index + i)
+                segment.render_world_objects(self.window)
+
+            p.render(self.window, base_segment)
+            p.render_hud(self.window)
+
+            if p.blood_alpha > 0:
+                p.render_blood(self.window)
+
+        for event in pygame.event.get():
+            if event.type == QUIT or\
+               (event.type == pygame.KEYDOWN and\
+                event.key == pygame.K_ESCAPE and\
+                s.FULLSCREEN):
+                pygame.quit()
+                sys.exit()
+
+        # Steering, acceleration.
+        keys = pygame.key.get_pressed()
+        p.set_acceleration(keys)
+        p.set_direction(keys)
+
+    def __pause_cycle(self):
+        pass
+
     def __title_screen(self):
         title_screen = ts.TitleScreen()
         pygame.mixer.music.load(os.path.join("lib", "mn84-theme.ogg"))
@@ -205,4 +220,3 @@ class Game:
 
             pygame.display.update()
             self.clock.tick(s.COUNTDOWN_FPS)
-
