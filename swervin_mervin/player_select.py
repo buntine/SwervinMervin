b16e0fc48877124271055b02f0dcfc1c4e5507b5
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-04-06 22:57:49 +1000

    Small refactor of in-progress player select

--- swervin_mervin/player_select.py
+++ swervin_mervin/player_select.py
@@ -8,22 +8,23 @@ class PlayerSelect():
     def __init__(self):
         self.selected   = 0
         self.finished   = False
+        self.background = pygame.image.load(os.path.join("lib", "player_select.png"))
         self.fonts      = {"title": pygame.font.Font(s.FONTS["retro_computer"], 38),
                            "name": pygame.font.Font(s.FONTS["retro_computer"], 22),
                            "details": pygame.font.Font(s.FONTS["retro_computer"], 14),
                            "stats": pygame.font.Font(s.FONTS["retro_computer"], 12)}
-        self.background = pygame.image.load(os.path.join("lib", "player_select.png"))
         
     def progress(self, window):
-        txt_title = self.fonts["title"].render("Player Select", 1, s.COLOURS["text"])
-        player    = s.PLAYERS[self.selected]
- 
+        txt_title     = self.fonts["title"].render("Player Select", 1, s.COLOURS["text"])
+        player        = s.PLAYERS[self.selected]
+        lpad          = 40
+        start_point   = (s.DIMENSIONS[0] / 2) + (lpad / 2)
+        step          = player["sprites"]["mugshot_small"]["width"]
+        large_mugshot = pygame.image.load(os.path.join("lib", player["sprites"]["mugshot_large"]["path"]))
+
         window.blit(self.background, (0, 0))
         window.blit(txt_title, ((s.DIMENSIONS[0] / 2) - (txt_title.get_size()[0] / 2), 10))
 
-        lpad        = 40
-        start_point = (s.DIMENSIONS[0] / 2) + (lpad / 2)
-        step        = player["sprites"]["mugshot_small"]["width"]
         for i, p in enumerate(s.PLAYERS):
             mugshot = pygame.image.load(os.path.join("lib", p["sprites"]["mugshot_small"]["path"]))
             x       = start_point + (i * (step + lpad))
@@ -31,13 +32,9 @@ class PlayerSelect():
 
             window.blit(mugshot, (x, y))
 
-        large_mugshot = pygame.image.load(os.path.join("lib", player["sprites"]["mugshot_large"]["path"]))
         window.blit(large_mugshot, (0, s.DIMENSIONS[1] - player["sprites"]["mugshot_large"]["height"]))
-
-        txt_name = self.fonts["name"].render(player["name"], 1, s.COLOURS["text"])
-        txt_city = self.fonts["details"].render(player["city"], 1, s.COLOURS["text"])
-        window.blit(txt_name, (start_point, 200))
-        window.blit(txt_city, (start_point, 228))
+        window.blit(self.fonts["name"].render(player["name"], 1, s.COLOURS["text"]), (start_point, 200))
+        window.blit(self.fonts["details"].render(player["city"], 1, s.COLOURS["text"]), (start_point, 228))
 
         for e in pygame.event.get():
             u.try_quit(e)
