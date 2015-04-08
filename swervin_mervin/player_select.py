be3a6865497425b99c7720d171f2213c8e800fb6
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-04-08 21:22:57 +1000

    Added basics of player selection sfx

--- swervin_mervin/player_select.py
+++ swervin_mervin/player_select.py
@@ -8,6 +8,7 @@ class PlayerSelect():
     def __init__(self):
         self.selected   = 0
         self.finished   = False
+        self.player_chosen = False
         self.background = pygame.image.load(os.path.join("lib", "player_select.png"))
         self.fonts      = {"title": pygame.font.Font(s.FONTS["fipps"], 38),
                            "name": pygame.font.Font(s.FONTS["retro_computer"], 18),
@@ -36,13 +37,26 @@ class PlayerSelect():
         window.blit(self.fonts["name"].render(player["name"], 1, s.COLOURS["text"]), (start_point, 200))
         window.blit(self.fonts["details"].render(player["city"], 1, s.COLOURS["text"]), (start_point, 228))
 
+        if self.player_chosen:
+            self.finalise_selection(player)
+
         for e in pygame.event.get():
             u.try_quit(e)
 
-            if e.type == pygame.KEYDOWN: 
+            if e.type == pygame.KEYDOWN and not self.player_chosen:
                 if e.key == pygame.K_LEFT and self.selected > 0:
                     self.selected -= 1
                 elif e.key == pygame.K_RIGHT and self.selected < len(s.PLAYERS) - 1:
                     self.selected += 1
                 elif e.key == pygame.K_RETURN:
-                    self.finished = True
+                    self.player_chosen = True
+
+    def finalise_selection(self, player):
+        pygame.mixer.music.load(os.path.join("lib", player["select_sfx"]))
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy():
+            continue
+
+        self.finished = True
