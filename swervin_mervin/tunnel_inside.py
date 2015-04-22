c596414d62f6d966073fb91b21d575ebd6d2573c
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-04-22 22:22:09 +1000

    Added right tunnel wall rendering

--- swervin_mervin/tunnel_inside.py
+++ swervin_mervin/tunnel_inside.py
@@ -8,6 +8,7 @@ class TunnelInside:
         self.quantifier = 3
 
     def render(self, window, coords, clip, coverage):
+        # Roof.
         s_width  = s.DIMENSIONS[0]
         s_height = int(s.TUNNEL_HEIGHT * coords["s"] * s.ROAD_WIDTH * self.quantifier)
         x        = 0
@@ -20,23 +21,28 @@ class TunnelInside:
         pygame.draw.rect(window, s.COLOURS["tunnel"], points)
 
         # Left wall.
-        l_bottom  = coverage[0].bottom["screen"]
-        ly_bottom = (s.DIMENSIONS[1] - l_bottom["y"])
-        l_x       = l_bottom["x"] - l_bottom["w"]
+        l_bottom = coverage[0].bottom["screen"]
 
-        points = [(0, ly_bottom),
-                  (l_x, ly_bottom),
-                  (l_x, y),
-                  (0, y)]
-        pygame.draw.polygon(window, s.COLOURS["tunnel"], points)
+        print l_bottom
+        if l_bottom["w"] < s.DIMENSIONS[0] and l_bottom["w"] > 0:
+            ly_bottom = (s.DIMENSIONS[1] - l_bottom["y"])
+            l_x       = l_bottom["x"] - l_bottom["w"]
+
+            points = [(0, ly_bottom),
+                      (l_x, ly_bottom),
+                      (l_x, y),
+                      (0, y)]
+            pygame.draw.polygon(window, s.COLOURS["tunnel"], points)
 
         # Right wall.
-        r_bottom  = coverage[2].bottom["screen"]
-        ry_bottom = (s.DIMENSIONS[1] - r_bottom["y"])
-        r_x       = r_bottom["x"] + r_bottom["w"]
+        r_bottom = coverage[2].bottom["screen"]
 
-        points = [(r_x, ry_bottom),
-                  (s.DIMENSIONS[0], ry_bottom),
-                  (s.DIMENSIONS[0], y),
-                  (r_x, y)]
-        pygame.draw.polygon(window, s.COLOURS["tunnel"], points)
+        if r_bottom["w"] < s.DIMENSIONS[0] and r_bottom["w"] > 0:
+            ry_bottom = (s.DIMENSIONS[1] - r_bottom["y"])
+            r_x       = r_bottom["x"] + r_bottom["w"]
+
+            points = [(r_x, ry_bottom),
+                      (s.DIMENSIONS[0], ry_bottom),
+                      (s.DIMENSIONS[0], y),
+                      (r_x, y)]
+            pygame.draw.polygon(window, s.COLOURS["tunnel"], points)
