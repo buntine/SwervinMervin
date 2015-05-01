c37d1fedaf7ec4701e828f142b77cfeca031f96b
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-01 23:41:51 +1000

    Experimenting with new track layout for Goldcoast

--- scripts/create_goldcoast_level.py
+++ scripts/create_goldcoast_level.py
@@ -11,9 +11,20 @@ segments += l.add_hill(50, 50, 50, 40, l.last_y(segments))
 segments += l.add_straight(500, l.last_y(segments))
 segments += l.add_hill(30, 30, 30, 20, l.last_y(segments))
 segments += l.add_straight(200, l.last_y(segments))
+segments += l.add_corner(30, 45, 40, 7, l.last_y(segments)) #1155
+segments += l.add_straight(90, l.last_y(segments))
+segments += l.add_hill(30, 30, 30, 0, l.last_y(segments)) #1335
+segments += l.add_straight(150, l.last_y(segments)) # 1485
 segments += l.add_corner(30, 45, 40, 7, l.last_y(segments))
-segments += l.add_straight(90, 0)
-segments += l.add_hill(30, 30, 30, 0, l.last_y(segments))
-segments += l.add_straight(150, 0)
+segments += l.add_straight(200, l.last_y(segments))
+segments += l.add_hill(80, 80, 80, 40, l.last_y(segments))
+segments += l.add_hill(20, 20, 20, 35, l.last_y(segments))
+segments += l.add_straight(50, l.last_y(segments))
+segments += l.add_hill(20, 20, 20, 20, l.last_y(segments))
+segments += l.add_corner(30, 45, 40, -7, l.last_y(segments))
+segments += l.add_corner(30, 45, 40, 4, l.last_y(segments))
+segments += l.add_straight(50, l.last_y(segments))
+segments += l.add_hill(20, 20, 20, 0, l.last_y(segments))
+segments += l.add_straight(100, l.last_y(segments))
 
 l.write("swervin_mervin/levels/tracks/{0}.csv".format(name), segments)
